04f913f883cf8f3f82da55b25ac5a5b74da8e9a3
TimAlex08 <tim.ponce.08@gmail.com>
2025-04-23 18:04:05 -0600

    Feature: Añadido inyección de dependencias con GetIt. Cambio en el main, primero se inicializan las dependencias. Tambén se cambió el proyecto inicial por un proyecto genérico mateApp

---
+++ lib/app/dependency_injection.dart
@@ -0,0 +1,29 @@
+// Dart Imports
+import 'dart:developer';
+
+// External Imports
+import 'package:get_it/get_it.dart';
+
+// Internal Imports
+import 'package:commonplace_book/app/commonplace_book/database/drift/app_database.dart';
+import 'package:commonplace_book/src/commonplace_book/notebook/infrastructure/adapters/drivens/notebook_sql_repository_adapter.dart';
+import 'package:commonplace_book/src/commonplace_book/notebook/infrastructure/ports/drivens/for_persiting_notebooks_port.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> setupDependencies() async {
+  try {
+   await _setupExternalDependencies();
+  } catch (e) {
+    log('Error setting up dependencies: $e');
+    rethrow;
+  }
+}
+
+Future<void> _setupExternalDependencies() async {
+  getIt.registerLazySingleton(() => AppDatabase()); 
+  
+  getIt.registerLazySingleton<ForPersitingNotebooksPort>(
+    () => NotebookDriftRepositoryAdapter(getIt<AppDatabase>())
+  );
+}
