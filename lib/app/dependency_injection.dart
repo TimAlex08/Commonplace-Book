c9eae59552eebc2dbfd8c29ab89cb80e25999dbe
TimAlex08 <tim.ponce.08@gmail.com>
2025-04-28 15:12:04 -0600

    Feature: Añadido casos de uso a la inyección de dependencias

--- lib/app/dependency_injection.dart
+++ lib/app/dependency_injection.dart
@@ -9,6 +9,19 @@ import 'package:commonplace_book/app/commonplace_book/database/drift/app_databas
 import 'package:commonplace_book/src/commonplace_book/notebook/infrastructure/adapters/drivens/notebook_drift_repository_adapter.dart';
 import 'package:commonplace_book/src/commonplace_book/notebook/infrastructure/ports/drivens/for_persiting_notebooks_port.dart';
 
+// UseCases
+import '../src/commonplace_book/notebook/application/notebook/commands/create_notebook_usecase.dart';
+import '../src/commonplace_book/notebook/application/notebook/commands/hard_delete_notebook_usecase.dart';
+import '../src/commonplace_book/notebook/application/notebook/commands/update_notebook_usecase.dart';
+import '../src/commonplace_book/notebook/application/notebook/observers/watch_all_notebooks_usecase.dart';
+import '../src/commonplace_book/notebook/application/notebook/observers/watch_notebook_by_id_usecase.dart';
+import '../src/commonplace_book/notebook/application/notebook/queries/get_all_notebooks_usecase.dart';
+import '../src/commonplace_book/notebook/application/notebook/queries/get_notebook_by_id_usecase.dart';
+import '../src/commonplace_book/notebook/infrastructure/adapters/drivers/notebook_manager_adapter.dart';
+import '../src/commonplace_book/notebook/infrastructure/ports/drivers/for_managing_notebooks.dart';
+
+
+
 final getIt = GetIt.instance;
 
 Future<void> setupDependencies() async {
@@ -26,4 +39,28 @@ Future<void> _setupExternalDependencies() async {
   getIt.registerLazySingleton<ForPersitingNotebooksPort>(
     () => NotebookDriftRepositoryAdapter(getIt<AppDatabase>())
   );
+  
+  // Casos de uso (Commands)
+  getIt.registerLazySingleton(() => CreateNotebookUseCase(getIt<ForPersitingNotebooksPort>()));
+  getIt.registerLazySingleton(() => UpdateNotebookUsecase(getIt<ForPersitingNotebooksPort>()));
+  getIt.registerLazySingleton(() => HardDeleteNotebookUsecase(getIt<ForPersitingNotebooksPort>()));
+  
+  // Casos de uso (Queries)
+  getIt.registerLazySingleton(() => GetAllNotebooksUsecase(getIt<ForPersitingNotebooksPort>()));
+  getIt.registerLazySingleton(() => GetNotebookByIdUseCase(getIt<ForPersitingNotebooksPort>()));
+  
+  // Casos de uso (Observers)
+  getIt.registerLazySingleton(() => WatchAllNotebooksUsecase(getIt<ForPersitingNotebooksPort>()));
+  getIt.registerLazySingleton(() => WatchNotebookByIdUsecase(getIt<ForPersitingNotebooksPort>()));
+  
+  // Adaptador de gestión de Notebooks UI (Driver Port) 
+    getIt.registerLazySingleton<ForManagingNotebooks>(() => NotebookManagerAdapter(
+    createNotebook: getIt<CreateNotebookUseCase>(),
+    updateNotebook: getIt<UpdateNotebookUsecase>(),
+    hardDeleteNotebook: getIt<HardDeleteNotebookUsecase>(),
+    getAllNotebooks: getIt<GetAllNotebooksUsecase>(),
+    getNotebookById: getIt<GetNotebookByIdUseCase>(),
+    watchAllNotebooks: getIt<WatchAllNotebooksUsecase>(),
+    watchNotebookById: getIt<WatchNotebookByIdUsecase>(),
+  ));
 }
